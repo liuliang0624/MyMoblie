a1ca9100c2a066c99a6b08cacba1290a04826660
Administrator <Administrator@DESKTOP-DS1ECV7>
2020-03-13 15:10:37 +0800

    添加项目文件。

---
+++ SmobilerApplication1/SmobilerForm1.Designer.cs
@@ -0,0 +1,52 @@
+﻿using Smobiler.Core;
+
+namespace SmobilerApplication1
+{
+    partial class SmobilerForm1 : Smobiler.Core.Controls.MobileForm
+    {
+        #region "SmobilerForm Designer generated code "
+
+        //SmobilerForm overrides dispose to clean up the component list.
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+
+        //NOTE: The following procedure is required by the SmobilerForm Designer
+        //It can be modified using the SmobilerForm Designer.  
+        //Do not modify it using the code editor.
+        [System.Diagnostics.DebuggerStepThrough()]
+        private void InitializeComponent()
+        {
+            this.button1 = new Smobiler.Core.Controls.Button();
+            this.button2 = new Smobiler.Core.Controls.Button();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.Text = "啦啦啦";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 30);
+            this.button2.Text = "button2";
+            // 
+            // SmobilerForm1
+            // 
+            this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.button1,
+            this.button2});
+            this.Name = "SmobilerForm1";
+
+        }
+        #endregion
+
+        private Smobiler.Core.Controls.Button button1;
+        private Smobiler.Core.Controls.Button button2;
+    }
+}
