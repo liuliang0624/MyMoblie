51cb28849fd5fee3cfd4eed96bf8bf7ac1a4d5c8
Administrator <Administrator@DESKTOP-DS1ECV7>
2020-03-13 15:16:08 +0800

    手机端第一次

--- SmobilerApplication1/SmobilerForm1.Designer.cs
+++ SmobilerApplication1/SmobilerForm1.Designer.cs
@@ -19,16 +19,8 @@ namespace SmobilerApplication1
         [System.Diagnostics.DebuggerStepThrough()]
         private void InitializeComponent()
         {
-            this.button1 = new Smobiler.Core.Controls.Button();
             this.button2 = new Smobiler.Core.Controls.Button();
             this.button3 = new Smobiler.Core.Controls.Button();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(150, 162);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 30);
-            this.button1.Text = "啦啦啦";
             // 
             // button2
             // 
@@ -47,15 +39,12 @@ namespace SmobilerApplication1
             // SmobilerForm1
             // 
             this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
-            this.button1,
             this.button2,
             this.button3});
             this.Name = "SmobilerForm1";
 
         }
         #endregion
-
-        private Smobiler.Core.Controls.Button button1;
         private Smobiler.Core.Controls.Button button2;
         private Smobiler.Core.Controls.Button button3;
     }
